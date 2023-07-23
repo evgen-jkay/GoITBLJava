c5e6dab18fe164b88fe07bf86546cbd9564dada1
evgen-jkay <evgenlandarenko@gmail.com>
2023-07-24 01:48:20 +0300

    add funcional

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static final List<String> cities = new ArrayList<>();
-    private static final Scanner scanner = new Scanner(System.in);
+    private static ArrayList<String> cities = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         fillCities();
@@ -13,35 +12,24 @@ public class Main {
 
     private static void gameLoop() {
         while (true) {
-            System.out.println("Введіть Ваше місто та натисніть Enter.");
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("exit")) {
+            if (input.equals("exit")) {
                 System.out.println("Дякую за гру!");
                 System.exit(0);
             }
 
-            char lastChar = Character.toLowerCase(input.charAt(input.length() - 1));
-            String nextCity = findCityWithFirstChar(lastChar);
+            char lastChar = input.charAt(input.length() - 1);
 
-            if (nextCity != null) {
-                System.out.println("Моє місто: " + nextCity);
-            } else {
-                System.out.println("Нажаль, не знайшли місто з відповідним першим символом.");
-                System.out.println("Ви ПЕРЕМОГЛИ!!!");
-                System.out.println("Ви можете продовжити гру далі або ввести команду exit для завершення гри...");
+            for (String city : cities) {
+                char firstChar = city.charAt(0);
+
+                if (Character.toLowerCase(lastChar) == Character.toLowerCase(firstChar)) {
+                    System.out.println("Моє місто: " + city);
+                    break;
+                }
             }
         }
-    }
-
-    private static String findCityWithFirstChar(char firstChar) {
-        for (String city : cities) {
-            if (Character.toLowerCase(city.charAt(0)) == firstChar) {
-                cities.remove(city); // Видаляємо місто зі списку, щоб уникнути повторного використання.
-                return city;
-            }
-        }
-        return null;
     }
 
     private static void fillCities() {
@@ -52,6 +40,6 @@ public class Main {
         cities.add("Харків");
         cities.add("Херсон");
         cities.add("Маріупіль");
-        cities.add("Запоріжжя");
+        cities.add("Запоріжя");
     }
 }
